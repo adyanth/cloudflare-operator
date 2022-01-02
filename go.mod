8fbb017e9c1d9e289c8f8a3fa47b76173f7dea79
Adyanth H <me@adyanth.dev>
2022-01-03 01:12:42 +0530

    :truck: Rename operator to controller due to lack of CRDs

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/adyanth/cloudflare-operator
+module github.com/adyanth/cloudflare-controller
 
 go 1.16
 
--- main.go
+++ main.go
@@ -31,7 +31,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	"github.com/adyanth/cloudflare-operator/controllers"
+	"github.com/adyanth/cloudflare-controller/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
