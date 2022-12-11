49aa1228cb7794c345e99a09a8209b90979a3147
Adyanth Hosavalike <me@adyanth.dev>
2022-12-10 23:36:25 -0800

    :bug: Rectify RBAC

--- controllers/tunnelbinding_controller.go
+++ controllers/tunnelbinding_controller.go
@@ -133,6 +133,7 @@ func (r *TunnelBindingReconciler) initStruct(ctx context.Context, tunnelBinding 
 //+kubebuilder:rbac:groups=networking.cfargotunnel.com,resources=clustertunnels/status,verbs=get
 //+kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch;update;patch
 //+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;update;patch
+//+kubebuilder:rbac:groups=core,resources=services,verbs=get
 //+kubebuilder:rbac:groups="",resources=events,verbs=create;patch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
